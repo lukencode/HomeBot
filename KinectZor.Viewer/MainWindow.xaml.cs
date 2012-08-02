a38294a586f1cae57fe917e18dd6f29a43f422dd
Luke Lowrey <lukenlowrey@gmail.com>
2012-08-03 00:00:50 +1000

    kinect is in the house

---
+++ KinectZor.Viewer/MainWindow.xaml.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using AForge.Imaging;
+using AForge.Vision.Motion;
+using Microsoft.Kinect;
+
+namespace KinectZor.Viewer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private KinectZorSensor _sensor = new KinectZorSensor();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            _sensor.MotionDetected += OnMotion;
+            _sensor.MotionStopped += MotionStopped;
+            //_sensor.FrameUpdated += null;
+
+            _sensor.Start();
+
+            imageDisplay.Source = _sensor.ColorBitmap;
+        }
+
+        private void OnMotion(object sender, MotionDetectedArgs e)
+        {
+            textMotion.Text = "MOTION";
+        }
+
+        private void MotionStopped(object sender, MotionDetectedArgs e)
+        {
+            textMotion.Text = "-";
+        }
+
+        private void SaveScreenShot()
+        {
+            if (_sensor == null)
+                return;
+
+            var folderPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyPictures), @"/kinect/");
+            var time = System.DateTime.Now.ToString("hh'-'mm'-'ss", CultureInfo.CurrentUICulture.DateTimeFormat);
+
+            if (!Directory.Exists(folderPath))
+                Directory.CreateDirectory(folderPath);
+
+            var filePath = Path.Combine(folderPath, "KinectSnapshot-" + time + ".png");
+            var fileStream = new FileStream(filePath, FileMode.Create);
+            TakeScreenShot(fileStream);
+        }
+
+        private void TakeScreenShot(Stream stream)
+        {
+            if (_sensor == null)
+                return;
+
+            var encoder = new PngBitmapEncoder();
+            encoder.Frames.Add(BitmapFrame.Create(_sensor.ColorBitmap));
+
+            try
+            {
+                using (stream)
+                {
+                    encoder.Save(stream);
+                }
+
+                Debug.WriteLine("Screenshot saved");
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Screenshot failed: " + ex.Message);
+            }
+        }
+
+        private void Window_Unloaded(object sender, RoutedEventArgs e)
+        {
+            _sensor.Stop();
+        }
+
+        private void btnScreenShot_Click(object sender, RoutedEventArgs e)
+        {
+            SaveScreenShot();
+        }
+    }
+}
---
+++ KinectZor/KinectZorSensor.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Kinect;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using AForge.Vision.Motion;
+using System.IO;
+using System.Windows;
+using AForge.Imaging;
+
+namespace KinectZor
+{
+    public class KinectZorSensor
+    {
+        private KinectSensor _sensor;
+        public WriteableBitmap ColorBitmap { get; private set; }
+        public byte[] ColorPixels { get; private set; }
+
+        private MotionDetector _detector = new MotionDetector(new SimpleBackgroundModelingDetector());
+        private bool _hasMotion;
+
+        public EventHandler<MotionDetectedArgs> MotionDetected;
+        public EventHandler<MotionDetectedArgs> MotionStopped;
+        public EventHandler FrameUpdated;
+
+        public KinectZorSensor()
+        {
+
+        }
+
+        public void Start()
+        {
+            if (KinectSensor.KinectSensors.Any())
+            {
+                _sensor = KinectSensor.KinectSensors.First();
+
+                if (_sensor.Status == KinectStatus.Connected)
+                {
+                    _sensor.ColorStream.Enable(ColorImageFormat.RgbResolution640x480Fps30);
+                    ColorPixels = new byte[_sensor.ColorStream.FramePixelDataLength];
+                    ColorBitmap = new WriteableBitmap(_sensor.ColorStream.FrameWidth, _sensor.ColorStream.FrameHeight, 96.0, 96.0, PixelFormats.Bgr32, null);
+                    _sensor.AllFramesReady += AllFramesReady;
+
+                    _sensor.Start();
+                }
+            }
+            else
+            {
+                throw new ApplicationException("No kinect detected");
+            }
+        }
+
+        public void Stop()
+        {
+            if (_sensor != null)
+            {
+                _sensor.Stop();
+                _sensor.AudioSource.Stop();
+            }
+        }
+
+        private void AllFramesReady(object sender, AllFramesReadyEventArgs e)
+        {
+            using (ColorImageFrame colorFrame = e.OpenColorImageFrame())
+            {
+                if (colorFrame == null)
+                    return;
+
+                colorFrame.CopyPixelDataTo(ColorPixels);
+
+                ColorBitmap.WritePixels(
+                    new Int32Rect(0, 0, ColorBitmap.PixelWidth, ColorBitmap.PixelHeight),
+                    ColorPixels,
+                    ColorBitmap.PixelWidth * sizeof(int),
+                    0);
+
+                DetectMotion();
+            }
+
+            if (FrameUpdated != null)
+                FrameUpdated(this, new EventArgs());
+        }
+
+        private void DetectMotion()
+        {
+            var frame = new UnmanagedImage(ColorBitmap.BackBuffer, ColorBitmap.PixelWidth, ColorBitmap.PixelHeight, ColorBitmap.BackBufferStride, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+
+            if (_detector.ProcessFrame(frame) > 0.02)
+            {
+                _hasMotion = true;
+                FireMotionDetected();
+            }
+            else
+            {
+                if (_hasMotion)
+                    FireMotionStopped();
+
+                _hasMotion = false;
+            }
+        }
+
+        private void FireMotionDetected()
+        {
+            if (MotionDetected != null)
+                MotionDetected(this, null);
+        }
+
+        private void FireMotionStopped()
+        {
+            if (MotionStopped != null)
+                MotionStopped(this, null);
+        }
+    }
+}
